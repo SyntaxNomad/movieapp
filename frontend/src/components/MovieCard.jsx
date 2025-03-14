5c53250ae0865d3e4bc47a2e5cc71bad7b2f0791
Mohamed Elfaki <moh.omer24@gmail.com>
2025-03-14 23:17:51 +0200

    movie finder

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,29 @@
+import "../css/MovieCard.css"
+function MovieCard({movie}) {
+    
+    function onFavoriteClick() {
+        alert('Clicked on favorite button');
+    }
+    return ( 
+    
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick= {onFavoriteClick}>Watch Trailer</button>
+            </div>
+
+
+    </div>
+    <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split("-")[0]}</p>
+    </div>
+    </div>
+
+
+
+
+    )
+}
+export default MovieCard
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"   
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2> No Favorite Movies Yet </h2>
+        <p>Find some movies you like and click on the favorite button to see them here.</p>
+    </div>
+
+}
+
+export default Favorites
